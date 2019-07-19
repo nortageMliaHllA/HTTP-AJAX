71f0b2425fdf2c2eb2d578caddab78fa2a38d182
AllHailMegatron <ahmadrthomp@gmail.com>
2019-07-18 20:08:28 -0400

    Data is rendering

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,17 +1,54 @@
-import Friends from './Friends';
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
-import './App.css';;
+import './App.css';
+import axios from 'axios';
+import FriendList from './FriendsList';
+import FriendForm from './FriendForm';
 
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      friends: [],
+        name: '',
+        age: '',
+        email: ''
+    };
+  }
 
-class App extends Component {
-  render() {
-    return (
-    <div className="App">
-      <Route exact path= "/" component={Friends} />
+  componentDidMount() {
+    axios
+        .get('http://localhost:5000/friends')
+        .then(response => { 
+        this.setState ({friends: response.data});
+        })
+        .catch(error => { 
+            console.log(error);
+  });
+}
+
+handleSubmit = () => {
+  const friend = { name: this.state.name, age: this.state.age, email: this.state.email };
+  axios
+    .post('http://localhost:5000/friends', friend)
+    .then((response) => {
+      this.setState({ friends: response.data}); 
+    })
+    .catch(error => {
+      console.log(error);
+});
+this.setState({ name: '', age: '', email: '' });
+};
+
+ handleNameChange = (e) => {
+  this.setState({ friend: e.target.value });
+};
+
+render() {
+  return (	    
+    <div className="App">	      
+        <FriendList friends={this.state.friends} />
+        <FriendForm handleNameChange={this.handleNameChange} handleSubmit={this.handleSubmit} />
     </div>
     );
   }
 }
-
-export default App;
--- friends/src/Friends.js
+++ friends/src/Friends.js
@@ -1,33 +1,16 @@
-import React, { Component } from 'react';
-import axios from "axios";
-import SingleFriends from './SingleFriends';
+import React from 'react';
 
 
-export default class Friends extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            friends: []
-        };
-    }
 
-    componentDidMount() {
-        axios
-            .get('http://localhost:5000/friends')
-            .then(response => { 
-            this.setState ({friends: response.data});
-            })
-            .catch(error => { 
-                console.error(error);
-    });
-}
-    render() {
-        return (
-            <div className='friends'>
-                {this.state.friends.map(friends => <SingleFriends id={friends.id} name={friends.name} age={friends.age} email={friends.email} />
-                )}
-            </div>
-        );
-    }
-}
-
+const Friend = (props) => {
+    return (
+      <div>
+        <div>{props.friend.name}</div>
+        <div>{props.friend.age}</div>
+        <div>{props.friend.email}</div>
+      </div>
+    );
+  };
+  
+   export default Friend;
+    
